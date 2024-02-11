dd835d6c99000d7e0cadf99c20d9fce57021b01a
Savva Voloshin <savva.voloshin@gmail.com>
2024-02-11 15:13:49 +0300

    approximation 0.6

--- flask_app.py
+++ flask_app.py
@@ -2,6 +2,7 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, request, jsonify
+from pprint import pprint 
 
 
 import httplib2 
@@ -52,26 +53,26 @@ spreadsheet = service.spreadsheets().create(body = {
 
 spreadsheetId = spreadsheet['spreadsheetId'] # сохраняем идентификатор файла
 
-driveService = apiclient.discovery.build('drive', 'v3', http = httpAuth) # Выбираем работу с Google Drive и 3 версию API
-access = driveService.permissions().create(
-    fileId = spreadsheetId,
-    body = {'type': 'user', 'role': 'writer', 'emailAddress': 'savva.voloshin@gmail.com'},  # Открываем доступ на редактирование
-    fields = 'id'
-).execute()
+# driveService = apiclient.discovery.build('drive', 'v3', http = httpAuth) # Выбираем работу с Google Drive и 3 версию API
+# access = driveService.permissions().create(
+#     fileId = spreadsheetId,
+#     body = {'type': 'user', 'role': 'writer', 'emailAddress': 'savva.voloshin@gmail.com'},  # Открываем доступ на редактирование
+#     fields = 'id'
+# ).execute()
 
-results = service.spreadsheets().values().batchUpdate(spreadsheetId = spreadsheetId, body = {
-        "valueInputOption": "USER_ENTERED", # Данные воспринимаются, как вводимые пользователем (считается значение формул)
-        "data": [
-            {"range": "Лист номер один!B2:D5",
-            "majorDimension": "ROWS",     # Сначала заполнять строки, затем столбцы
-            "values": [
-                        ["Ячейка B2", "Ячейка C2", "Ячейка D2"], # Заполняем первую строку
-                        ['25', "=6*6", "=sin(3,14/2)"],
-                    ]}
-        ]
-    }).execute()
+# results = service.spreadsheets().values().batchUpdate(spreadsheetId = spreadsheetId, body = {
+#         "valueInputOption": "USER_ENTERED", # Данные воспринимаются, как вводимые пользователем (считается значение формул)
+#         "data": [
+#             {"range": "Лист номер один!B2:D5",
+#             "majorDimension": "ROWS",     # Сначала заполнять строки, затем столбцы
+#             "values": [
+#                         ["Ячейка B2", "Ячейка C2", "Ячейка D2"], # Заполняем первую строку
+#                         ['25', "=6*6", "=sin(3,14/2)"],
+#                     ]}
+#         ]
+#     }).execute()
 
-print('https://docs.google.com/spreadsheets/d/' + spreadsheetId)
+# print('https://docs.google.com/spreadsheets/d/' + spreadsheetId)
 
 def myinit():
     global cache
@@ -81,7 +82,7 @@ def myinit():
 def hello_world():
 
     with open(THIS_FOLDER / 'tmp.log', 'a+') as f:
-        f.write('log file should be initialized\n')
+        pprint('log file should be initialized', f)
     
     conn = mysql.connect()
     cursor =conn.cursor()
@@ -112,36 +113,11 @@ CREATE TABLE IF NOT EXISTS `tblsample` (
 @app.route('/bitrix24', methods=['POST'])
 def handle_bitrix24():
 
-    with open(THIS_FOLDER / 'tmp.log', 'a+') as f:
-        f.write('handle_bitrix24 \n')
-
     # data=request.json
 
     with open(THIS_FOLDER / 'tmp.log', 'a+') as f:
-        f.write('data should appears below \n')
-        f.write(str(request.form))
-
-        # f.write(jsonify(data).get_data(as_text=True))
-    # замените на ваш вебхук для доступа к Bitrix24
-    webhook = "https://b24-onzqts.bitrix24.ru/rest/1/glt6iy0bi3ihay6s/"
-    b = Bitrix(webhook)
-
-    # список сделок в работе, включая пользовательские поля
-    deals = b.get_all(
-        'crm.deal.list',
-        params={
-            'select': ['*', 'UF_*'],
-            'filter': {'CLOSED': 'N'}
-    })
-
-    contacts = b.get_by_ID(
-    'crm.deal.contact.items.get',
-    [d['ID'] for d in deals])
-
-    with open(THIS_FOLDER / 'tmp.log', 'a+') as f:
-        f.write('contacts data should appears below \n')
-        f.write(str(contacts))
-
+        pprint('handle_bitrix24', f)
+        pprint(request.form, f)
 
     values = [
         [1,2,3,4,5,6,7,8,9]
@@ -149,8 +125,10 @@ def handle_bitrix24():
     
     body = {'values': values}
 
-    results = service.spreadsheets().values().append(
-        spreadsheetId=spreadsheetId, range="Лист номер один!B2:D5", valueInputOption="RAW", body=body).execute()
+    # results = service.spreadsheets().values().append(
+    #     spreadsheetId=spreadsheetId,
+    #     range="Лист номер один!B2:D5",
+    #     valueInputOption="RAW", body=body).execute()
     
     return jsonify({"code":200})
 
--- test.py
+++ test.py
@@ -1,61 +1,74 @@
-# from fast_bitrix24 import Bitrix
+from pprint import pprint 
+from fast_bitrix24 import Bitrix
 
-# # замените на ваш вебхук для доступа к Bitrix24
-# webhook = "https://b24-onzqts.bitrix24.ru/rest/1/glt6iy0bi3ihay6s/"
-# b = Bitrix(webhook)
-# # список сделок в работе, включая пользовательские поля
-# # deals = b.get_all(
-# #     'crm.deal.list',
-# #     params={
-# #         'select': ['*', 'UF_*'],
-# #         'filter': {'CLOSED': 'N'}
-# # })
+# замените на ваш вебхук для доступа к Bitrix24
+webhook = "https://b24-onzqts.bitrix24.ru/rest/1/glt6iy0bi3ihay6s/"
+b = Bitrix(webhook)
+# список сделок в работе, включая пользовательские поля
+deal_id = '14'
+deal = b.get_all(
+    'crm.deal.list',
+    params={
+        'select': ['ID', 'CONTACT_ID', 'COMMENTS'],
+        'filter': {'ID': deal_id}
+})
 
-# # contacts = b.get_by_ID(
-# # 'crm.deal.contact.items.get',
-# # [d['ID'] for d in deals])
+# pprint(deal)
+comments = deal[0]['COMMENTS']
+contact_id = deal[0]['CONTACT_ID']
 
-# # contact = b.get_by_ID('crm.deal.contact.fields', '20')
-# # contact = b.get_by_ID('crm.deal.contact.fields')
-# # contact = b.get_all('crm.contact.list')
+contact = b.get_all(
+    'crm.contact.list',
+    params={
+        'select': ['LAST_NAME', 'NAME', 'PHONE',],
+        'filter': {'ID': '2'}
+})
 
-# # cc = b.get_all(
-# #     'crm.contact.list',
-# #     params={
-# #         'select': ['*', 'PHONE']
-# # })
+name1 = contact[0]['LAST_NAME']
+name2 = contact[0]['NAME']
+phone = contact[0]['PHONE'][0]['VALUE']
 
-# # print(deals)
-# # print()
-# # print(contacts)
-# # print()
-# print(cc)
+# pprint(contact)
 
-import yaml
+pprint([comments,phone,name1,name2])
 
-# app.config['MYSQL_DATABASE_USER'] = 'root'
-# app.config['MYSQL_DATABASE_PASSWORD'] = '181483331264'
-# app.config['MYSQL_DATABASE_DB'] = 'mysql'
-# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+# contacts = b.get_by_ID(
+# 'crm.deal.contact.items.get',
+# [d['ID'] for d in deals])
 
-db_config = [
-    {
-        'Details': {
-        'MYSQL_DATABASE_USER' : 'root',
-        'MYSQL_DATABASE_PASSWORD': '181483331264',
-        'MYSQL_DATABASE_DB' : 'mysql',
-        'MYSQL_DATABASE_HOST': 'localhost',
-        }
-    }
-]
+# contact = b.get_by_ID('crm.deal.contact.fields', '20')
+# contact = b.get_by_ID('crm.deal.contact.fields')
+# contact = b.get_all('crm.contact.list')
 
-with open("db_config.yaml", 'w') as yamlfile:
-    data = yaml.dump(db_config, yamlfile)
-    print("Write successful")
+# cc = b.get_all(
+#     'crm.contact.list',
+#     params={
+#         'select': ['*', 'PHONE']
+# })
 
-with open("db_config.yaml", "r") as yamlfile:
-    data = yaml.load(yamlfile, Loader=yaml.FullLoader)
-    print("Read successful")
+# print(deals)
+# print()
+# print(contacts)
+# print()
+# pprint(deal)
 
-print(data)
-print(data[0]['Details']['MYSQL_DATABASE_USER'])
+# import yaml
+
+# db_config = [
+#     {
+#         'Details': {
+#         'MYSQL_DATABASE_USER' : 'root',
+#         'MYSQL_DATABASE_PASSWORD': '181483331264',
+#         'MYSQL_DATABASE_DB' : 'mysql',
+#         'MYSQL_DATABASE_HOST': 'localhost',
+#         }
+#     }
+# ]
+
+# with open("db_config.yaml", 'w') as yamlfile:
+#     data = yaml.dump(db_config, yamlfile)
+#     print("Write successful")
+
+# with open("db_config.yaml", "r") as yamlfile:
+#     data = yaml.load(yamlfile, Loader=yaml.FullLoader)
+#     print("Read successful")
